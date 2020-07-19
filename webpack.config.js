6d62e46cc87b7a9dd54ea8f02a241c3c3e3c538d
Valentin Kaelin <kalanehd@gmail.com>
2020-07-19 15:27:41 +0200

    chore: add symfony encore adonis provider

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const Encore = require('@symfony/webpack-encore')
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
+}
+
+Encore
+  .setOutputPath('public/build/')
+  .setPublicPath('/build')
+  .addEntry('js/app', './resources/ts/app.ts')
+  .addStyleEntry('css/app', './resources/scss/app.scss')
+  .splitEntryChunks()
+  .enableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .enableIntegrityHashes(Encore.isProduction())
+  .enableSassLoader()
+  .enableTypeScriptLoader()
+
+module.exports = Encore.getWebpackConfig()
